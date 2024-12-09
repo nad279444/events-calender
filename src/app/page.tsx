4a02f83ff520b8aa1f9cb50d2496ac51d766778e
nad279444 <nardfrimpong@gmail.com>
2024-12-09 18:12:09 +0000

    created crud functionalities for events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function HomePage (){
-  return null
+import { Button } from "@/components/ui/button"
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+export default async function HomePage() {
+  const { userId } =  await auth()
+  if (userId != null) redirect("/events")
+
+  return (
+    <div className="text-center container my-4 mx-auto">
+      <h1 className="text-3xl mb-4">Fancy Home Page</h1>
+      <div className="flex gap-2 justify-center">
+        <Button asChild>
+          <SignInButton />
+        </Button>
+        <Button asChild>
+          <SignUpButton />
+        </Button>
+        <UserButton />
+      </div>
+    </div>
+  )
 }
